c8d0eaa217305e4781a8ed3f6ef33452696948cc
MateChaitanya <chaitanyamate02@gmail.com>
2025-02-13 13:02:01 +0530

    Initial commit

--- src/jdbcInsertDemo/InsertDemo.java
+++ src/jdbcInsertDemo/InsertDemo.java
@@ -1,12 +1,24 @@
 package jdbcInsertDemo;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
 
 public class InsertDemo {
 
 	public static void main(String[]args)throws Exception{
+		
+		//Step 1 - Load the Driver
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		DriverManager.getConnection("jdbc:mysql://localhost:3306/db1","root","Chaitu@123");
-		System.out.print("Success");
+		
+		
+		//Step 2 - Create a Connection 
+		Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3306/db1","root","Chaitu@123");
+		
+		
+		//Step 3- Create Statement 
+		PreparedStatement ps = con.prepareStatement("INSERT INTO register (name, email, password, phone) VALUES ('Jailin', 'jailin@example.com', 'jailin123', '9234558460')");
+		ps.executeUpdate();
+
 	}
 }
